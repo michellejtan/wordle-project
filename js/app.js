952594ce7fc8a26fb2296662343cff11b95e0078
michellejtan <michelle0223@gmail.com>
2025-02-25 14:26:49 -0800

    accept user typed letters and insert letters to cells

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const validWords = ["apple", "table", "chair", "store", "world", "plant", "drink", "flame"];
 let secretWord = validWords[Math.floor(Math.random() * validWords.length)];
 let attempts = 6;
+let nextLetter = 0;
+let guess = '';
 let currentRow = 0;
 let gameOver = false;
+console.log(secretWord);
 
 document.getElementById('attempts-left').textContent = attempts;
 
@@ -18,15 +21,56 @@ function initializeGameBoard() {
         for (let j = 0; j < 5; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.setAttribute('contenteditable', true);
+            // cell.setAttribute('contenteditable', true);
             cell.setAttribute('maxlength', 1);
             row.appendChild(cell);
         }
-
         gameBoard.appendChild(row);
     }
 }
 console.log();
+
+// Accept User Input
+document.addEventListener("keyup", (e) => {
+
+    if (attempts === 0) {
+        return
+    }
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter()
+        return
+    }
+
+    if (pressedKey === "Enter") {
+        checkGuess()
+        return
+    }
+
+    let found = pressedKey.match(/[a-z]/gi); // case-insensitive search of all occurrences
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
+
+function insertLetter (pressedKey) {
+    if (nextLetter === 5) {
+        return
+    }
+    pressedKey = pressedKey.toLowerCase()
+
+    let row = document.getElementsByClassName("row")[6 - attempts]
+    let box = row.children[nextLetter]
+    console.log(box);
+    box.textContent = pressedKey
+    box.classList.add("filled-cell")
+    guess += pressedKey;
+    console.log("Guess: " + guess);
+    nextLetter += 1
+}
 
 // Submit guess with Enter key
 document.addEventListener('keydown', function (event) {
