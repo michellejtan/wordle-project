ebb278c9f87556c6101ed4ee312b213aa480e7fc
michellejtan <michelle0223@gmail.com>
2025-02-25 17:14:56 -0800

    handle keyboard input through clicking on the virtual keyboard buttons

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ function initializeGameBoard() {
 }
 console.log();
 
-// Accept User Input
+// Accept User Input through physical keyboard events
 document.addEventListener("keyup", (e) => {
 
     if (attempts === 0) {
@@ -38,7 +38,7 @@ document.addEventListener("keyup", (e) => {
     }
 
     let pressedKey = String(e.key)
-    if (pressedKey === "Backspace" && nextLetter !== 0) {
+    if (pressedKey === "Backspace" && nextLetter !== 0) { // nothing to delete until something is inserted
         deleteLetter()
         return
     }
@@ -56,6 +56,36 @@ document.addEventListener("keyup", (e) => {
     }
 })
 
+// handle keyboard input through clicks on virtual keyboard buttons 
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+    const target = e.target
+
+    if (!target.classList.contains("keyboard-button")) {
+        return
+    }
+    let key = target.textContent
+
+    if (key === "Del") {
+        key = "Backspace"
+    } 
+
+    document.dispatchEvent(new KeyboardEvent("keyup", {'key': key})); // simulates the action of pressing a key on a physical keyboard
+})
+
+function removeLastLetter(str) {
+    return str.slice(0, -1);
+}
+
+function deleteLetter() {
+    let row = document.getElementsByClassName("row")[6 - attempts]
+    let box = row.children[nextLetter - 1]
+    box.textContent = ""
+    box.classList.remove("filled-box")
+    guess = removeLastLetter(guess)
+    console.log("Guess: " + guess);
+    nextLetter -= 1
+}
+
 function insertLetter (pressedKey) {
     if (nextLetter === 5) {
         return
@@ -71,6 +101,7 @@ function insertLetter (pressedKey) {
     console.log("Guess: " + guess);
     nextLetter += 1
 }
+
 
 // Submit guess with Enter key
 document.addEventListener('keydown', function (event) {
